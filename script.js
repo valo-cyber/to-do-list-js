fa73499a71b79288c6f761576b9d0fb34489bd4f
valo-cyber <abasval558@gmail.com>
2022-07-29 14:49:03 -0500

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
- 
+function sleep(){
+    document.getElementsByClassName("val")
+}
+function add(){
+    document.getElementsByClassName("task1")
+}
+function me(){
+    document.getElementsByClassName("task")
+    let paragraph = document.createElement("p")
+
+}
+
+addEventListener("click")
+
+let sleep = document.getElementsByClassName("val");
+let add = document.getElementsByClassName("task1");
+let me = document.getElementsByClassName("task");
+
+sleep.addEventListener("click", function(){
+    let paragraph = document.createElement("p")
+    paragraph.innerText = tea.value;
+    add.appendChild(paragraph);
+})
