6a31d08489289b4467ffa5ab9dd248ada14cd4b6
valo-cyber <abasval558@gmail.com>
2022-07-30 18:19:45 -0500

    third commit

--- script.js
+++ script.js
@@ -32,26 +32,30 @@ input.addEventListener("keydown", function(event){
 function addItem(){
     let divParent = document.createElement("div")
     let divChild = document.createElement("div")
-    let checkIcon = document.createElement("i")
-    let trashIcon = document.createElement("i")
+    let checkIcon = document.createElement("button")
+    let trashIcon = document.createElement("button")
 
-    divParent.className = "item";
+    divParent.className = "item"; 
+    divChild.className = "ite";
     divParent.innerHTML = "<div>" + input.value + "<div>";
 
-    checkIcon.className = "fas fa-check-square";
-    checkIcon.style.color = "lightgray";
+    checkIcon.className = "check";
+    checkIcon.style.color = "white";
+    checkIcon.style.marginRight="10px"
     checkIcon.addEventListener("click", function(){
         checkIcon.style.color = "limegreen"
     })
     divChild.appendChild(checkIcon)
+    checkIcon.textContent = "Checked"
     
-    trashIcon.className = "fas fa-trash";
-    trashIcon.style.color = "darkgray";
+    trashIcon.className = "delete";
+    trashIcon.style.color = "white";
+    trashIcon.textContent = "Delete"
     trashIcon.addEventListener("click", function(){
         divParent.remove();
     })
     divChild.appendChild(trashIcon)
-
+    
     divParent.appendChild(divChild)
 
     toDoItems.appendChild(divParent)
